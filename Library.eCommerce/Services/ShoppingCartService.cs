d04a0163fac3c717b56138eec7d21b2cd5911bca
gabbyva5 <166071945+gabbyva5@users.noreply.github.com>
2025-02-02 13:32:53 -0500

    Create ShoppingCartService.cs

---
+++ Library.eCommerce/Services/ShoppingCartService.cs
@@ -0,0 +1,14 @@
+using Spring2025_Samples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.eCommerce.Services
+{
+    public class ShoppingCartService
+    {
+        
+    }
+}
