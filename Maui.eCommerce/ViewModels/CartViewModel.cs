8a8730bd0e58d10b35fdbcf17b4c7cb076312ba8
Gabrielle <gabby.5681vicy18@gmail.com>
2025-04-20 16:34:13 -0400

    error fixed

---
+++ Maui.eCommerce/ViewModels/CartViewModel.cs
@@ -0,0 +1,44 @@
+using Library.eCommerce.Services;
+using Spring2025_Samples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.eCommerce.ViewModels
+{
+    public class CartViewModel
+    {
+        public int Quantity
+        {
+            get
+            {
+                return Model?.Quantity ?? 0;
+            }
+            set
+            {
+                if (Model != null && Model.Quantity != value)
+                {
+                    Model.Quantity = value;
+                }
+            }
+        }
+
+        public Product? Model { get; set; }
+
+        public Product? AddOrUpdate()
+        {
+            return ShoppingCartService.Current.AddOrUpdate(Model);
+        }
+
+        public CartViewModel() {
+            Model = new Product();
+        }
+
+        public CartViewModel(Product? model)
+        {
+            Model = model;
+        }
+    }
+}
