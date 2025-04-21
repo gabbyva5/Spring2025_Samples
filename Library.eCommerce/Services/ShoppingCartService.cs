235aeae5c9d7849624b872eef8037948ef98fa4f
Gabrielle <gabby.5681vicy18@gmail.com>
2025-04-21 00:07:45 -0400

    minor fixes

--- Library.eCommerce/Services/ShoppingCartService.cs
+++ Library.eCommerce/Services/ShoppingCartService.cs
@@ -25,7 +25,7 @@ namespace Library.eCommerce.Services
             {
                 lock(instanceLock)
                 {
-                    if (instance == null)
+                    if(instance == null)
                     {
                         instance = new ShoppingCartService();
                     }
@@ -35,62 +35,62 @@ namespace Library.eCommerce.Services
             }
         }
 
-        public Product? AddOrUpdate(Product product)
+        public Product? AddOrUpdateCart(Product product)
         {
-            var inventoryProd= ProductServiceProxy.Current.GetById(product.Id);
+            var inventoryProd = ProductServiceProxy.Current.GetById(product.Id);
             var prodInCart = GetById(product.Id);
 
-            if(inventoryProd != null)
-            {    
-                int cartQuan= prodInCart?.Quantity ?? 0;
-                if(inventoryProd.Quantity + cartQuan< product.Quantity)
-                    product.Quantity= inventoryProd.Quantity + cartQuan;    //if user wants more than is available, just give them the rest
+            if(inventoryProd == null) 
+                return null;
 
+            if(prodInCart == null)     //triggered by AddtoCart and item isn't alr in can
+            {
+                if(product.Quantity == 0)
+                    return null;
 
-                if(product.Quantity==0)
-                {
-                    if(prodInCart != null)
-                    {
-                        inventoryProd.Quantity+= prodInCart.Quantity;   //if quan is 0, prod will be removed from cart but will remain in inventory
-                        Cart.Items?.Remove(prodInCart);
-                    }
-                    return prodInCart;
-                }
-                else if(prodInCart != null) 
-                {
-                    int diff;
+                if(inventoryProd.Quantity < product.Quantity)
+                    product.Quantity = inventoryProd.Quantity;
 
-                    if(product.Quantity < prodInCart.Quantity)    
-                    {
-                        diff= prodInCart.Quantity- product.Quantity;
-                        prodInCart.Quantity=product.Quantity;
-                        inventoryProd.Quantity+=diff;
-                    }
-                    else    //if the # in cart is less than new quantity
-                    {
-                        diff= product.Quantity- prodInCart.Quantity;
-                        prodInCart.Quantity= product.Quantity;
-                        inventoryProd.Quantity-=diff;
-                    }
-
-                    return prodInCart;
-                }
-                else
-                {
-                    Cart.Items?.Add(product);
-                    inventoryProd.Quantity-=product.Quantity;
-                    return product;
-                }
+                Cart.Items?.Add(product);
+                inventoryProd.Quantity-= product.Quantity;
+                return product;
             }
+            else    //triggered by AddtoCart when item is alr in cart or when quantity is updated from shopping list
+            {
+                if (product.Quantity == 0)
+                {
+                    inventoryProd.Quantity += prodInCart.Quantity;
+                    Cart.Items?.Remove(prodInCart);     //remove from cart but still in inventory
+                    return prodInCart;
+                }
 
-            else return null;    //return null if attempted product does not exist
+                int diff;
+                if(product.Quantity>prodInCart.Quantity)    //more than in cart
+                {
+                    if(inventoryProd.Quantity + prodInCart.Quantity< product.Quantity)
+                        product.Quantity= inventoryProd.Quantity + prodInCart.Quantity;
+                    
+                    diff= product.Quantity - prodInCart.Quantity;
+                    prodInCart.Quantity += diff;
+                    inventoryProd.Quantity -= diff;
+                }
+                else    //also catches if product.Quantity==prodInCart.Quantity
+                {
+                    diff= prodInCart.Quantity-product.Quantity;
+                    prodInCart.Quantity -= diff; 
+                    inventoryProd.Quantity += diff; 
+                }
+
+                return prodInCart;
+            }
         }
+
 
         public Product? Delete(int id)
         {
-            var cartProd = Cart.Items?.FirstOrDefault(p => p.Id == id);
+            var cartProd= Cart.Items?.FirstOrDefault(p => p.Id == id);
             var inventoryProd= ProductServiceProxy.Current.Products.FirstOrDefault(p => p?.Id == id);
-            if (cartProd != null && inventoryProd != null)
+            if(cartProd != null && inventoryProd != null)
             {
                 inventoryProd.Quantity+=cartProd.Quantity;  //add product quantity back to inventory
                 Cart.Items?.Remove(cartProd);
